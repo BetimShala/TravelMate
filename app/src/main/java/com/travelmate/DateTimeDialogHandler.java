e7fc64864b8842480461586e01f445fd9d60bc1c
BetimShala <betim_sh97@live.com>
2018-05-13 16:01:58 +0200

    date and time dialog handler added

---
+++ app/src/main/java/com/travelmate/DateTimeDialogHandler.java
@@ -0,0 +1,27 @@
+package com.travelmate;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.util.TimeUtils;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+/**
+ * Created by Betim on 5/12/2018.
+ */
+
+public class DateTimeDialogHandler extends DialogFragment {
+
+    public Dialog onCreateDialog(Bundle savedInstaceState){
+
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog dialog;
+        dialog = new TimePickerDialog(getActivity(),(TimePickerDialog.OnTimeSetListener)getActivity(),hour,minute, android.text.format.DateFormat.is24HourFormat(getActivity()));
+        return dialog;
+    }
+}
